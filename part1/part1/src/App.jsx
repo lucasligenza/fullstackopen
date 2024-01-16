f0f39a39368b1046ee9e8b4d2988440b3c0972d4
lucasligenza <lucasligenza@gmail.com>
2024-01-16 13:15:48 -0500

    1.2

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -5,14 +5,21 @@ const Header = (props) => {
       </div>
   )
 }
-
 const Content = (props) => {
-return (
-    <div>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </div>
+  
+  return (
+    <>
+      <Part part={props.part1} exercise={props.exercises1}/>
+      <Part part={props.part2} exercise={props.exercises2}/>
+      <Part part={props.part3} exercise={props.exercises3}/>
+    </>
+    
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
   )
 }
 
@@ -38,9 +45,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
       <Total total={exercises1+exercises2+exercises3}/>
     </div>
   )
